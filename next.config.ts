4f3ea3814aab1235be49e3f496bf0350f4518a3c
Abhiraman Kuntimaddi <abhiraman21696@icloud.com>
2026-01-20 22:51:09 +0100

    Fixing end points

--- next.config.ts
+++ next.config.ts
@@ -2,24 +2,13 @@
 
 const tunnelUrl = process.env.NEXT_PUBLIC_STRAPI_TUNNEL_URL || "";
 const tunnelHost = tunnelUrl.replace("https://", "");
+
 const nextConfig = {
 	images: {
 		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "cdn.sanity.io",
-				pathname: "/images/**",
-			},
-			{
-				protocol: "https",
-				hostname: "images.unsplash.com",
-				pathname: "/**",
-			},
-			{
-				protocol: "https",
-				hostname: "placehold.co",
-				pathname: "/**",
-			},
+			{ protocol: "https", hostname: "cdn.sanity.io", pathname: "/images/**" },
+			{ protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
+			{ protocol: "https", hostname: "placehold.co", pathname: "/**" },
 			{
 				protocol: "https",
 				hostname: tunnelHost,
@@ -32,6 +21,18 @@ const nextConfig = {
 			{
 				source: "/studio-cms/:path*",
 				destination: `${tunnelUrl}/:path*`,
+			},
+			{
+				source: "/admin/:path*",
+				destination: `${tunnelUrl}/admin/:path*`,
+			},
+			{
+				source: "/api/:path*",
+				destination: `${tunnelUrl}/api/:path*`,
+			},
+			{
+				source: "/i18n/:path*",
+				destination: `${tunnelUrl}/i18n/:path*`,
 			},
 		];
 	},
