6a14f6594a55de44566072372c46989a986ea2df
Abhiraman Kuntimaddi <abhiraman21696@icloud.com>
2026-01-20 22:55:24 +0100

    Testing the cms endpoint

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,24 @@
 
 const tunnelUrl = process.env.NEXT_PUBLIC_STRAPI_TUNNEL_URL || "";
 const tunnelHost = tunnelUrl.replace("https://", "");
-
 const nextConfig = {
 	images: {
 		remotePatterns: [
-			{ protocol: "https", hostname: "cdn.sanity.io", pathname: "/images/**" },
-			{ protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
-			{ protocol: "https", hostname: "placehold.co", pathname: "/**" },
+			{
+				protocol: "https",
+				hostname: "cdn.sanity.io",
+				pathname: "/images/**",
+			},
+			{
+				protocol: "https",
+				hostname: "images.unsplash.com",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "placehold.co",
+				pathname: "/**",
+			},
 			{
 				protocol: "https",
 				hostname: tunnelHost,
@@ -20,19 +31,7 @@ const nextConfig = {
 		return [
 			{
 				source: "/studio-cms/:path*",
-				destination: `${tunnelUrl}/:path*`,
-			},
-			{
-				source: "/admin/:path*",
-				destination: `${tunnelUrl}/admin/:path*`,
-			},
-			{
-				source: "/api/:path*",
-				destination: `${tunnelUrl}/api/:path*`,
-			},
-			{
-				source: "/i18n/:path*",
-				destination: `${tunnelUrl}/i18n/:path*`,
+				destination: `${tunnelUrl}/studio-cms/:path*`,
 			},
 		];
 	},
